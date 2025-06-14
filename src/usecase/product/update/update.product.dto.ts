0497353dc2d7cb2b9f536bca3fed439b3a96f2ab
Felipe Pereira <felipe.pereira@tectoy.com>
2025-06-13 23:16:09 -0300

    feat: add UpdateProductDto and UpdateProductOutputDto for product update functionality

---
+++ src/usecase/product/update/update.product.dto.ts
@@ -0,0 +1,11 @@
+export interface UpdateProductDto {
+  id: string;
+  name: string;
+  price: number;
+}
+
+export interface UpdateProductOutputDto {
+  id: string;
+  name: string;
+  price: number;
+}
