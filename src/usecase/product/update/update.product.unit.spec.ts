be46bad2ada8f2c1332b0363892aaed6be6b9d1a
Felipe Pereira <felipe.pereira@tectoy.com>
2025-06-13 23:16:17 -0300

    feat: add unit tests for UpdateProductUseCase to validate product update functionality

---
+++ src/usecase/product/update/update.product.unit.spec.ts
@@ -0,0 +1,66 @@
+import Product from '../../../domain/product/entity/product'
+import UpdateProductUseCase from './update.product.usecase'
+
+const product = new Product('123', 'Product A', 100);
+
+const MockRepository = () => {
+  return {
+    find: jest.fn().mockReturnValue(Promise.resolve(product)),
+    findAll: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+  };
+};
+
+describe("Unit Test UpdateProductUseCase", () => {
+  it("should update a product", async () => {
+    const productRepository = MockRepository();
+    const usecase = new UpdateProductUseCase(productRepository);
+
+    await productRepository.create(product);
+
+    const input = {
+      id: "123",
+      name: "Product A Updated",
+      price: 150,
+    };
+    const output = await usecase.execute(input);
+    expect(output).toEqual({
+      id: "123",
+      name: "Product A Updated",
+      price: 150,
+    });
+  });
+
+  it("should not update a product with missing name", async () => {
+    const productRepository = MockRepository();
+    const usecase = new UpdateProductUseCase(productRepository);
+
+    await productRepository.create(product);
+
+    const input = {
+      id: "123",
+      name: "",
+      price: 150,
+    };
+
+    await expect(usecase.execute(input)).rejects.toThrow("Name is required");
+  });
+
+  it("should not update a product with missing price", async () => {
+    const productRepository = MockRepository();
+    const usecase = new UpdateProductUseCase(productRepository);
+
+    await productRepository.create(product);
+
+    const input = {
+      id: "123",
+      name: "Product A Updated",
+      price: -1,
+    };
+
+    await expect(usecase.execute(input)).rejects.toThrow(
+      'Price must be greater than zero',
+    );
+  });
+});
