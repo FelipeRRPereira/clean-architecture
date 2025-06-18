19c73d023805a9f5c8ddbef02175fb386c8b8b21
Felipe Pereira <felipe.pereira@tectoy.com>
2025-06-17 22:48:47 -0300

    fix: update error messages in Product unit tests to include prefix

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -3,20 +3,28 @@ import Product from './product'
 describe('Product unit tests', () => {
   it('Should throw error when id is empty', () => {
     expect(() => {
-      const product = new Product('', 'Product 1', 100)
-    }).toThrow('Id is required')
+      new Product('', 'Product 1', 100)
+    }).toThrow('product: Id is required')
   })
 
   it('Should throw error when name is empty', () => {
     expect(() => {
-      const product = new Product('123', '', 100)
-    }).toThrow('Name is required')
+      new Product('123', '', 100)
+    }).toThrow('product: Name is required')
   })
 
-  it('Should throw error when prive is less than zero', () => {
+  it('Should throw error when price is less than zero', () => {
     expect(() => {
-      const product = new Product('123', 'Product', -1)
-    }).toThrow('Price must be greater than zero')
+      new Product('123', 'Product', -1)
+    }).toThrow('product: Price must be greater than zero')
+  })
+
+  it('Should throw error when name is empty and price is less than zero', () => {
+    expect(() => {
+      new Product('123', '', -1)
+    }).toThrow(
+      'product: Name is required,product: Price must be greater than zero',
+    )
   })
 
   it('Should change name', () => {
