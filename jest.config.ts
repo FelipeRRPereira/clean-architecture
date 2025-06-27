8ab6188ba013ce15b5c68e19abf0735ce2c56a0d
Felipe Pereira <felipe.pereira@tectoy.com>
2025-06-26 22:47:54 -0300

    fix: update transformIgnorePatterns to allow jstoxml transformation

--- jest.config.ts
+++ jest.config.ts
@@ -180,10 +180,9 @@ const config: Config = {
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  // transformIgnorePatterns: [
-  //   "/node_modules/",
-  //   "\\.pnp\\.[^\\/]+$"
-  // ],
+  transformIgnorePatterns: [
+    'node_modules/(?!(jstoxml)/)', // permite que jstoxml seja transformado
+  ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
